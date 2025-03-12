85303bd5e41a8fdd9471307402c7417106d347d5
Juan-Carlos-SA <juansanchezaguirre7@gmail.com>
2025-03-12 07:15:33 -0600

    ¿registro

--- hotelia/src/components/inicio/Menu/Menu.js
+++ hotelia/src/components/inicio/Menu/Menu.js
@@ -9,7 +9,7 @@ export function Menu() {
                 <Navbar.Brand as={Link} to="/">HOTELIA</Navbar.Brand>
                 <Nav className="me-auto">
                     <Link to="/iniciosesion" className="btn btn-outline-info">Inicio de sesión</Link>
-                    <Link to="/registro" className="btn btn-outline-info">Registro uwu</Link>
+                    <Link to="/registro" className="btn btn-outline-info">Registro</Link>
                     <Link to="/catalogo" className="btn btn-outline-info">Catalogo</Link>
                     <Link to="/metodopago" className="btn btn-outline-info">Metodo de pago</Link>
 
