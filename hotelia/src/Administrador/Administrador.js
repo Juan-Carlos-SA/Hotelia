8a4fcc8b68cd4e10801af0c79e795c8fcf4b7fc2
Min-creador38 <marinaguerramoreno398@gmail.com>
2025-03-26 13:36:34 -0600

    .

--- hotelia/src/Administrador/Administrador.js
+++ hotelia/src/Administrador/Administrador.js
@@ -3,7 +3,7 @@ import React from 'react'
 export function Administrador() {
   return (
     <div>
-        Administrador
-        </div>
+        <meta className='viewport' content='width=device-width, user-scalable=no, initial-scalable=1.0,maximum-scale=1.0, minimum-scale=1.0'/>
+     </div>
   )
 }
--- hotelia/src/components/inicio/Menu/Menu.js
+++ hotelia/src/components/inicio/Menu/Menu.js
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 
 export function Menu() {
     return (
-        <Navbar bg="primary" data-bs-theme="dark">
+        <Navbar bg="primary">
+            <meta className='viewport' content='width=device-width, user-scalable=no, initial-scalable=1.0,maximum-scale=1.0, minimum-scale=1.0'/>
             <Container>
+
                 <Navbar.Brand as={Link} to="/"> HOTELIA </Navbar.Brand>
                 <Nav className="me-auto">
                     <Link to="/Iniciosesion" className="btn btn-outline-info"> Inicio de sesión </Link>
