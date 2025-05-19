c2a5b3dc224354efa537155ba1b10884733b0f8c
Juan-Carlos-SA <juansanchezaguirre7@gmail.com>
2025-05-19 09:35:17 -0600

    api edicion/imagenesalb

--- api/models/producto.models.js
+++ api/models/producto.models.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductoSchema = new mongoose.Schema({
     nombre: { type: String},
     precio: { type: Number},
-    cantidad: { type: Number},
+    cantidad: { type: String},
     unidad: { type: String},
     imagep: { type: String },
     createdAT: { type: Date, default: Date.now },
